3505b88e2f68141a6510c2cdabbb73bc93f5624b
Nithin K Joy <nithinjoy121212@gmail.com>
2025-10-25 14:56:28 +0530

    Added Content into Application client class

--- Sources/code/application/ApplicationClient.swift
+++ Sources/code/application/ApplicationClient.swift
@@ -3,11 +3,15 @@ import Foundation
 
 public class ApplicationClient {
     private let config: ApplicationConfig
+    
+    public let content: Content
 
     public let user: User
 
     public init(config: ApplicationConfig) {
         self.config = config
+        
+        content = Content(config: config)
         
         user = User(config: config)
     }
